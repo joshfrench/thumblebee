ec34cc61e652193ebd1a96c3dfa3438a4bcaeecc
jfrench2 <jfrench2@22698fe0-d021-0410-a9a5-dc0f9d26e629>
2007-07-05 22:09:20 +0000

    Added toggle to ride form
    git-svn-id: http://vitamin-j.com/svn/apps/rideboard/trunk@343 22698fe0-d021-0410-a9a5-dc0f9d26e629

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var Nicely = {};
+Nicely = {
+	show_ride_form: function() {
+		Effect.toggle($('ride_form'), 'blind');
+		$('toggle_link').innerHTML = '<a href="#" onclick="Nicely.hide_ride_form(); return false;">Cancel</a>';
+	},
+	
+	hide_ride_form: function() {
+		Effect.toggle($('ride_form'), 'blind');
+		$('toggle_link').innerHTML = '<a href="#" onclick="Nicely.show_ride_form(); return false;">Add a ride</a>';	
+	}
+}
