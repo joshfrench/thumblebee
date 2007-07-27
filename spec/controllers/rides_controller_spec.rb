ea33d8068bd27bd2ab6cf86292cff190ee810f62
jfrench2 <jfrench2@22698fe0-d021-0410-a9a5-dc0f9d26e629>
2007-07-27 20:42:27 +0000

    Specced rides_controller
    git-svn-id: http://vitamin-j.com/svn/apps/rideboard/trunk@428 22698fe0-d021-0410-a9a5-dc0f9d26e629

--- spec/controllers/rides_controller_spec.rb
+++ spec/controllers/rides_controller_spec.rb
@@ -85,31 +85,41 @@ describe "A POST to update" do
   
   before(:each) do
     @ride = mock("ride")
-    Ride.should_receive(:find_by_auth).and_return(@ride)
     @event = mock("event")
     @ride.stub!(:event).and_return(@event)
     @ride.stub!(:update_attributes!).and_return(@ride)
   end
   
   it "with valid params should find proper ride" do
+    Ride.should_receive(:find_by_auth).and_return(@ride)
     post :update, { :event_id => "foo", :id => 'bar', :ride => {} }
     assigns[:ride].should equal(@ride)
   end
   
   it "with valid params should set flash" do
+    Ride.should_receive(:find_by_auth).and_return(@ride)
     post :update, { :event_id => "foo", :id => 'bar', :ride => {} }
     flash[:message].should_not be_nil
   end
   
   it "with valid params should redirect" do
+    Ride.should_receive(:find_by_auth).and_return(@ride)
     post :update, { :event_id => "foo", :id => 'bar', :ride => {} }
     response.should be_redirect
   end
   
   it "with invalid params should raise an error" do
-    @ride.stub!(:update_attributes!).and_raise(ActiveRecord::RecordInvalid)
+    Ride.should_receive(:find_by_auth).and_return(@ride)
+    @ride.should_receive(:update_attributes!).and_raise(ActiveRecord::RecordInvalid)
     post :update, { :event_id => "foo", :id => 'bar', :ride => {} }
     response.should render_template(:show)
+  end
+  
+  it "with a non-existant ride should raise an error" do
+    Ride.should_receive(:find_by_auth).and_return(nil)
+    lambda {
+      post :update, { :event_id => "foo", :id => 'bar', :ride => {} }
+    }.should raise_error(ActiveRecord::RecordNotFound)
   end
     
 end
