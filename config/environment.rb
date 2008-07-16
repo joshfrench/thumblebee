cb51b5fda8b16e86bf67089e01583f737273968c
Josh French <josh@vitamin-j.com>
2008-07-15 22:07:36 -0400

    Remove vendored gems

--- config/environment.rb
+++ config/environment.rb
@@ -24,26 +24,8 @@ Rails::Initializer.run do |config|
   
 end
 
-# Boot those Gems
-standard_dirs = ['rails', 'plugins']
-gems          = Dir[File.join(RAILS_ROOT, "vendor/**")]
-
-if gems.any?
-  gems.each do |dir|
-    next if standard_dirs.include?(File.basename(dir))
-    lib = File.join(dir, 'lib')
-    if File.directory?(lib)
-      $LOAD_PATH.unshift(lib) 
-      Dependencies.load_paths << lib
-      Dependencies.load_once_paths << lib
-    end
-  end
-end
-
 require 'redgreen' unless (ENV['TM_RUBY'] || ENV['RAILS_ENV'] == 'production')
-require 'ruby-debug'
-Debugger.start
-require 'json'
+
 Date::DATE_FORMATS[:nice] = "%B %d, %Y"
 ActionMailer::Base.default_url_options[:host] = 'vitamin-j.com/rideboard'
 ExceptionNotifier.exception_recipients = %w(josh@vitamin-j.com)
