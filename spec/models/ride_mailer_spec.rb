8aca2ea89d1f7dbe87a2804582d60b729c59c4e9
Josh French <josh@vitamin-j.com>
2008-07-27 21:10:39 -0400

    Add ride mailer spec

--- spec/models/ride_mailer_spec.rb
+++ spec/models/ride_mailer_spec.rb
@@ -1,5 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe RideMailer do
-  it "should send a new-ride email"
+  fixtures :all
+  
+  before do
+    ActionMailer::Base.delivery_method = :test 
+    ActionMailer::Base.perform_deliveries = true 
+    ActionMailer::Base.deliveries = []
+    @ride = rides(:available)
+    @event = events(:active)
+  end
+  
+  it "should send a new-ride email" do
+    response = RideMailer.create_new_ride(@ride)
+    response.to.should   include(@ride.email)
+    response.from.should include(@event.email)
+    response.body.should match(/#{@ride.driver}/)
+    response.body.should match(/#{@event.name}/)
+    response.body.should match(/#{@event.slug}\/rides\/#{@ride.auth}/)
+  end
 end
