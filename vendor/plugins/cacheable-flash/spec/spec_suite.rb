8d047fa2583efe7f1da18e24f8d63e264b41db1f
Josh French <josh@vitamin-j.com>
2008-07-27 21:35:42 -0400

    Update cacheble_flash

--- vendor/plugins/cacheable-flash/spec/spec_suite.rb
+++ vendor/plugins/cacheable-flash/spec/spec_suite.rb
@@ -1,12 +1,12 @@
-class TestSuite
+class SpecSuite
   def run
     dir = File.dirname(__FILE__)
-    Dir["#{dir}/**/*_test.rb"].each do |file|
+    Dir["#{dir}/**/*_spec.rb"].each do |file|
       require file
     end
   end
 end
 
 if $0 == __FILE__
-  TestSuite.new.run
+  SpecSuite.new.run
 end
