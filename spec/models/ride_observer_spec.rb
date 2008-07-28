299c050c317957ecdcb9fcfc0f2cdf08bcaac7b9
Josh French <josh@vitamin-j.com>
2008-07-27 20:57:35 -0400

    Add ride_observer spec

--- spec/models/ride_observer_spec.rb
+++ spec/models/ride_observer_spec.rb
@@ -1,5 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe RideObserver do
-  it "should send an email when a ride is added"
+  fixtures :all
+  
+  it "should send an email when a ride is added" do
+    RideMailer.should_receive(:deliver_new_ride)
+    events(:active).rides.create(rides(:available).attributes)
+  end
+
 end
