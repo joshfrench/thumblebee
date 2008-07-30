a6f983c20757ae403c5d96213da1bb8c9483fe45
Josh French <josh@vitamin-j.com>
2008-07-29 22:46:38 -0400

    Whitespace OCD

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.resources :events do |event|
-    event.resources :rides, :path_prefix => ':event_id',
-                    :name_prefix => ''
+    event.resources :rides, :path_prefix => ':event_id', :name_prefix => ''
   end
   
   map.with_options :controller => 'sessions' do |session|
-    session.new_session ':event_id/rides/:id/login', :action => 'new',
-                                           :conditions => { :method => :get }
-    session.session ':event_id/rides/:id/login', :action => 'create',
-                                       :conditions => { :method => :post }
+    session.new_session ':event_id/rides/:id/login', :action => 'new', :conditions => { :method => :get }
+    session.session ':event_id/rides/:id/login', :action => 'create', :conditions => { :method => :post }
   end
   
   map.default ':id', :controller => 'events', :action => 'show'
