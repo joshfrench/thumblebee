5cd11aa694da1178c6e01833fc07ee3708cb7022
Josh French <josh@vitamin-j.com>
2008-07-30 08:34:36 -0400

    JS cleanup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,11 @@
-var Nicely = {};
-Nicely = {
-	highlight_row: function(id) {
-		$('rides').getElements('td.ride_'+id).highlight('#8A79AA')
-	}
-};
-
 window.addEvent('domready', function() {
+	rowHighlighter = function(id,color) {
+		$('rides').getElements('td.ride_'+id).highlight(color);
+	}
+
 	var myAccordion = new Accordion($('rides'), 'h3.ride_header', 'div.ride_body', {
 		opacity: false, duration: 200, show: -1, alwaysHide: true
 	});
-	
-	var status = {
-		'true': '<strong><a href="#" id="toggle_link">Cancel</a></strong>',
-		'false': '<strong>Giving a ride?</strong> Fill out the <a href="#" id="toggle_link">add a ride</a> form.'
-	};
 	
 	var formSlider = new Fx.Slide('ride_form', { duration: 600 });
 	
