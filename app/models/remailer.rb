aae2ece969bb34d2a597d7acb644f97aad39907e
Josh French <josh@vitamin-j.com>
2008-08-02 13:49:19 -0400

    Generated remailer class

---
+++ app/models/remailer.rb
@@ -0,0 +1,4 @@
+class Remailer < ActionMailer::Base
+  
+
+end
---
+++ test/unit/remailer_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class RemailerTest < ActionMailer::TestCase
+  tests Remailer
+  # replace this with your real tests
+  def test_truth
+    assert true
+  end
+end
