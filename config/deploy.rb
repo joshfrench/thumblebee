f3b993c2618efc15363937b8b6dd6a34bc8a0e8e
Josh French <josh@vitamin-j.com>
2008-08-02 18:13:30 -0400

    Rewrote deloy for git [#2 status:open]

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,136 +1,28 @@
-# This defines a deployment "recipe" that you can feed to capistrano
-# (http://manuals.rubyonrails.com/read/book/17). It allows you to automate
-# (among other things) the deployment of your application.
+set :application, "thumblebee.com"
+set :scm, :git
+set :repository, "git@thumblebee.com:rideboard.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-# =============================================================================
-# REQUIRED VARIABLES
-# =============================================================================
-# You must always specify the application and repository for every recipe. The
-# repository must be the URL of the repository you want this recipe to
-# correspond to. The deploy_to path must be the path on each machine that will
-# form the root of the application path.
+role :web, application
+role :app, application
+role :db,  application, :primary => true
 
-set :application, "rideboard"
-set :repository, "svn+ssh://jfrench2@vitamin-j.com/home/jfrench2/svn/apps/rideboard/trunk"
+set :deploy_to, "/var/www/thumblebee.com"
+set :user,      "josh"
+set :keep_releases, 5 
 
-# =============================================================================
-# ROLES
-# =============================================================================
-# You can define any number of roles, each of which contains any number of
-# machines. Roles might include such things as :web, or :app, or :db, defining
-# what the purpose of each machine is. You can also specify options that can
-# be used to single out a specific subset of boxes in a particular role, like
-# :primary => true.
-
-role :web, "vitamin-j.com"
-role :app, "vitamin-j.com"
-role :db,  "vitamin-j.com", :primary => true
-
-# =============================================================================
-# OPTIONAL VARIABLES
-# =============================================================================
-set :deploy_to, "/home/jfrench2/apps/rideboard" # defaults to "/u/apps/#{application}"
-set :user,      "jfrench2"            # defaults to the currently logged in user
-set :use_sudo,  false
-set :chmod755, %w(app config db lib public vendor script tmp public/dispatch.cgi public/dispatch.fcgi public/dispatch.rb)
-set :checkout, "export"
-set :keep_releases, 2            # teeny tiny disk space!
-# set :scm, :darcs               # defaults to :subversion
-# set :svn, "/path/to/svn"       # defaults to searching the PATH
-# set :darcs, "/path/to/darcs"   # defaults to searching the PATH
-# set :cvs, "/path/to/cvs"       # defaults to searching the PATH
-# set :gateway, "gate.host.com"  # default to no gateway
-
-# =============================================================================
-# SSH OPTIONS
-# =============================================================================
-# ssh_options[:keys] = %w(/path/to/my/key /path/to/another/key)
-# ssh_options[:port] = 25
 default_run_options[:pty] = true
 
-# =============================================================================
-# TASKS
-# =============================================================================
-# Define tasks that run on all (or only some) of the machines. You can specify
-# a role (or set of roles) that each task should be executed on. You can also
-# narrow the set of servers to a subset of a role by specifying options, which
-# must match the options given for the servers to select (like :primary => true)
-
-desc <<DESC
-An imaginary backup task. (Execute the 'show_tasks' task to display all
-available tasks.)
-DESC
-task :backup, :roles => :db, :only => { :primary => true } do
-  # the on_rollback handler is only executed if this task is executed within
-  # a transaction (see below), AND it or a subsequent task fails.
-  on_rollback { run "rm /tmp/dump.sql" }
-
-  run "mysqldump -u theuser -p thedatabase > /tmp/dump.sql" do |ch, stream, out|
-    ch.send_data "thepassword\n" if out =~ /^Enter password:/
-  end
+Dir.new(File.join(File.dirname(__FILE__), 'recipes')).entries.sort.each do |entry|
+  require "config/recipes/#{entry}" if entry =~ /\.rb$/
 end
 
-# Tasks may take advantage of several different helper methods to interact
-# with the remote server(s). These are:
-#
-# * run(command, options={}, &block): execute the given command on all servers
-#   associated with the current task, in parallel. The block, if given, should
-#   accept three parameters: the communication channel, a symbol identifying the
-#   type of stream (:err or :out), and the data. The block is invoked for all
-#   output from the command, allowing you to inspect output and act
-#   accordingly.
-# * sudo(command, options={}, &block): same as run, but it executes the command
-#   via sudo.
-# * delete(path, options={}): deletes the given file or directory from all
-#   associated servers. If :recursive => true is given in the options, the
-#   delete uses "rm -rf" instead of "rm -f".
-# * put(buffer, path, options={}): creates or overwrites a file at "path" on
-#   all associated servers, populating it with the contents of "buffer". You
-#   can specify :mode as an integer value, which will be used to set the mode
-#   on the file.
-# * render(template, options={}) or render(options={}): renders the given
-#   template and returns a string. Alternatively, if the :template key is given,
-#   it will be treated as the contents of the template to render. Any other keys
-#   are treated as local variables, which are made available to the (ERb)
-#   template.
-
-desc "Demonstrates the various helper methods available to recipes."
-task :helper_demo do
-  # "setup" is a standard task which sets up the directory structure on the
-  # remote servers. It is a good idea to run the "setup" task at least once
-  # at the beginning of your app's lifetime (it is non-destructive).
-  setup
-
-  buffer = render("maintenance.rhtml", :deadline => ENV['UNTIL'])
-  put buffer, "#{shared_path}/system/maintenance.html", :mode => 0644
-  sudo "killall -USR1 dispatch.fcgi"
-  run "#{release_path}/script/spin"
-  run "rm #{shared_path}/system/maintenance.html"
-end
-
-# You can use "transaction" to indicate that if any of the tasks within it fail,
-# all should be rolled back (for each task that specifies an on_rollback
-# handler).
-
-desc "Restart fcgi processes"
-task :restart, :roles => :app do
-  run "pkill -9 -u jfrench2 -f dispatch.fcgi"
-end
-
-task :spinner, :roles => :app do
-   run "rm -rf /home/#{user}/public_html/rideboard;ln -s #{current_path}/public /home/#{user}/public_html/rideboard"
-end
-
-desc "Link in the production database.yml and fix permissions" 
-task :after_update_code do
-  run "rm #{current_path}/tmp/sessions/*"
-  run "mv #{release_path}/config/environment.rb #{release_path}/config/environment.rb~"
-  run "ruby -e 'readlines.each{ |l| if( l.include?(%q(RAILS_ENV)) ) then puts l[/[^# ]+.*/] else puts l end } ' #{release_path}/config/environment.rb~ > #{release_path}/config/environment.rb "
-  run "find #{release_path}/public -type d -exec chmod 0755 {} \\;"
-  run "find #{release_path}/public -type f -exec chmod 0644 {} \\;"
-  run "chmod 0755 #{release_path}/public/dispatch.fcgi"
-  run "chmod 0755 #{release_path}/public/dispatch.cgi"
-  run "chmod 0755 #{release_path}/public/dispatch.rb"
-  
-  spinner
+[:god, :passenger, :haml, :chronic, 
+ :packet, :backupgem, { :rails => '2.1' }].each do |gem|
+if gem.is_a?(Hash)
+  name, version = gem.shift
+  depend :remote, :gem, name, version
+else
+  depend :remote, :gem, gem, '>=0'
 end
