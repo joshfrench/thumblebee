0ca8106458c4a85a2c269dc5de1c09ad603602d4
Josh French <josh@vitamin-j.com>
2008-08-02 18:13:58 -0400

    Added god conf [#1 status:open]

---
+++ config/god.rb
@@ -0,0 +1,43 @@
+RAILS_ROOT = '/var/www/thumblebee.com/current'
+
+God::Contacts::Email.message_settings = {
+  :from => 'god@thumblebee.com'
+}
+
+God::Contacts::Email.server_settings = {
+  :address        => "localhost",
+  :port           => 25
+}
+
+God.contact(:email) do |c|
+  c.name  = 'josh'
+  c.email = 'josh@vitamin-j.com'
+end
+
+################
+# BACKGROUNDRB #
+################
+God.watch do |w|
+  w.name = 'backgroundrb'
+  w.interval = 1.minute
+  w.grace = 1.minute
+  w.start = "#{RAILS_ROOT}/script/backgroundrb start"
+  w.stop  = "#{RAILS_ROOT}/script/backgroundrb stop"
+  w.behavior :clean_pid_file
+  w.pid_file = File.join(RAILS_ROOT, 'tmp/backgroundrb_11006.pid')
+  
+  w.start_if do |start|
+    start.condition(:process_running) do |c|
+      c.running = false
+      c.notify = 'josh'
+    end
+  end
+  
+  w.restart_if do |start|
+    restart_condition(:memory_usage) do |c|
+      c.above = 50.megabytes
+      c.times = [3,5]
+      c.notify = 'josh'
+    end
+  end
+end
