b5538da1443062f546d686249ca4f2945c5342a4
Josh French <josh@vitamin-j.com>
2008-08-03 15:43:04 -0400

    Remove reply-to headers

--- app/models/remailer.rb
+++ app/models/remailer.rb
@@ -2,7 +2,6 @@ class Remailer < ActionMailer::Base
   def forward(ride,email)
     @recipients      = ride.email
     @from            = email.from.first
-    @reply_to        = email.from.first
     @subject         = email.subject
     @body[:message]  = email.body
     @body[:orig_to]  = email.to.first
--- app/models/ride_mailer.rb
+++ app/models/ride_mailer.rb
@@ -2,7 +2,6 @@ class RideMailer < ActionMailer::Base
   def new_ride(ride)
     @recipients     = "#{ride.email}"
     @from           = "#{ride.event.contact} <#{ride.event.email}>"
-    @reply_to       = "#{ride.event.contact} <#{ride.event.email}>"
     @subject        = "Thanks for posting to the ride board"
     @sent_on        = Time.now
     @body[:ride]    = ride
