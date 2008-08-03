fffbc2fea566f2706cea5ae10c628045a657f191
Josh French <josh@vitamin-j.com>
2008-08-03 16:32:43 -0400

    Write sqlite3-friendly deploy:migrations task [#15 state:resolved]

--- config/recipes/sqlite3.rb
+++ config/recipes/sqlite3.rb
@@ -5,18 +5,26 @@ Capistrano::Configuration.instance.load do
       desc "Make shared DB file"
       task :touch_production do
         config = thumblebee.get_db_config
-        run "mkdir -p #{shared_path}/db"
-        run "touch #{shared_path}/#{config[:database]}"
+        run "mkdir -p #{File.join shared_path, File.dirname(config[:database])}"
+        run "touch #{File.join shared_path, config[:database]}"
       end
       after 'deploy:setup', 'deploy:db:touch_production'
       
       desc "Symlink production DB to shared location"
       task :symlink do
         config = thumblebee.get_db_config
-        run "ln -sf #{shared_path}/#{config[:database]} #{current_path}/#{config[:database]}"
+        run "ln -sf #{File.join shared_path, config[:database]} #{File.join current_path, config[:database]}"
       end
       after 'deploy:symlink', 'deploy:db:symlink'
       
+    end
+    
+    task :migrations do
+      set :migrate_target, :current
+      update_code
+      symlink
+      migrate
+      restart
     end
   end
 end
