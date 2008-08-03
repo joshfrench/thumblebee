f59949608ba00f5e632b78c3145f810afcac1dc1
Josh French <josh@vitamin-j.com>
2008-08-03 13:10:29 -0400

    Add dynamic error templates [#12 state:resolved]

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,31 @@
-# Filters added to this controller apply to all controllers in the application.
-# Likewise, all the methods added will be available for all controllers.
-
 class ApplicationController < ActionController::Base
   include HoptoadNotifier::Catcher
   include Authentication
+
+  private
+  
+    def render_404
+      respond_to do |type|
+        type.html { render :template => "errors/404", :status => "404 Not Found"}
+        type.all  { render :nothing => true, :status => "404 Not Found" }
+      end
+    end
+
+    def render_500
+      respond_to do |type|
+        type.html { render :template => "errors/500", :status => "500 Error" }
+        type.all  { render :nothing => true, :status => "500 Error" }
+      end
+    end
+
+    def rescue_action_in_public(exception)
+      case exception
+        when *[ActiveRecord::RecordNotFound, ActionController::UnknownController,
+               ActionController::UnknownAction, ActionController::RoutingError]
+          render_404
+        else          
+          render_500
+      end
+    end
+    
 end
