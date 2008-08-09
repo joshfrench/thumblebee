dbbcdeac858b67898048608a6c0b9bdf9b7e7b16
Josh French <josh@vitamin-j.com>
2008-08-09 19:02:29 -0400

    Watch pid when starting/stopping backgroundrb

--- config/recipes/backgroundrb.rb
+++ config/recipes/backgroundrb.rb
@@ -3,15 +3,16 @@ Capistrano::Configuration.instance.load do
     desc "Start the backgroundrb server"
     task :start do
       sudo "god start backgroundrb"
+      logger.info 'Waiting for backgroundrb to start...'
+      sleep 10 while capture("find #{shared_path}/pids -name backgroundrb_11006.pid").empty?
     end
     after 'deploy:restart', 'backgroundrb:start'
     
     desc "Stop the backgroundrb server"
     task :stop do
       sudo "god stop backgroundrb"
-      logger.info "Pause while backgroundrb goes down"
-      60.times { print '.' ; sleep 1 }
-      puts
+      logger.info 'Waiting for backgroundrb to stop...'
+      sleep 10 while capture("find #{shared_path}/pids -name backgroundrb_11006.pid").any?
     end
     before 'deploy', 'backgroundrb:stop'
   end
